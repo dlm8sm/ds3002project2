cdc2a4451b7b9c3214efd9191bb738226e946132
dlm8sm <78705257+dlm8sm@users.noreply.github.com>
2021-05-10 22:52:12 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,17 @@
 # test.py file in lambda function #
-
 import requests
+import csv
 
 def call_pylenin(event=None, context=None):
     r = requests.get("https://4feaquhyai.execute-api.us-east-1.amazonaws.com/api/pi")
     rson = r.json()
-    return rson
+    # get json data from api
+    factor = rson['factor']
+    # factor value from api
+    pi = rson['pi']     
+    # pi value from api
+    time = rson['time']
+    # time value from api
+    list = [factor, pi, time]
+    
+    return list
